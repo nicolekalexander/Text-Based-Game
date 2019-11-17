a964345287a405b4c8e496b037ce96624272b418
Nicole <nkalexander1@verizon.net>
2019-11-17 16:18:05 -0500

    cleaned up code and added comments

--- script.py
+++ script.py
@@ -1,27 +1,32 @@
+#import packages
 import json
 import time
 
+#variables
 file_path = "story.json"
-#variable story that opens and reads whatever file is at the file_path
 story = open(file_path, "r").read()
 story_json = json.loads(story)
 scenes = story_json["scenes"]
 
+#function definition
 def get_scene(scene_id):
     for scene in scenes:
         if (scene["id"] == scene_id):
             print(scene["text"] + "\n")
             time.sleep(3)
-            print("What are you going to do?")
-            time.sleep(2)
             for choice in (scene["choices"]):
                 print(str(choice["scene_id"]) + ". " + choice["text"])
             time.sleep(2)
             selected_scene = input("Enter your choice.")
             get_scene(selected_scene)
 
-print(story_json["title"] + " by " + story_json["author"] + "\n")
-time.sleep(2)
-print(story_json["intro"] + "\n")
-time.sleep(2)
+def get_intro():
+    print(story_json["title"] + " by " + story_json["author"] + "\n")
+    time.sleep(2)
+    print(story_json["intro"] + "\n")
+    time.sleep(2)
+
+
+#starting game
+get_intro()
 get_scene(1)
